9885d37a6c754e5204b52cb368e445fd55db4572
Nicolas Sam <samiam151@gmail.com>
2020-07-02 15:03:56 -0400

    wip;

--- scripts/muscle_groups.py
+++ scripts/muscle_groups.py
@@ -1,6 +1,21 @@
 from bs4 import BeautifulSoup
-import requests
-# import helpers
+import requests, json
+
+def parse_ul(data, ul):
+    if ul is None:
+        return ul
+
+    a_tag = ul.find("a")
+    if (a_tag): 
+        data[a_tag.text] = []
+
+    all_uls = ul.find("ul")
+    if len(all_uls) > 0:
+        for _ul in all_uls:
+            return parse_ul(data, _ul)
+
+    return parse_ul(data, ul.find("ul"))
+
 
 # Get main HTML
 html = requests.get("https://exrx.net/Lists/Directory/")
@@ -12,11 +27,13 @@ baseRow = mainHtml.find("div", attrs={"class", "container"}).find("div", attrs={
 sm6_sections = baseRow.find_all("div", attrs={"class", "col-sm-6"}, recursive=False)
 
 for section in sm6_sections:
-    section_ul = section.find("ul", recursive=False)
-    section_title = section_ul
-    print(section_ul.contents)
+    data = {}
+    section_uls = section.find_all("ul", recursive=False)
+   
+    for section_ul in section_uls:
+        print(parse_ul(data, section_ul))
+
+
 
 if __name__ == "__main__":
-    for section in sm6_sections:
-        # print(type(section)) 
-        pass
+    print(data)
